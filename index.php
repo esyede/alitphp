975b72721ef1c95eb8d4caca900b3d9b16641040
esyede <suyadi.1992@gmail.com>
2017-10-16 08:03:40 +0700

    update

--- index.php
+++ index.php
@@ -1,9 +1,13 @@
 <?php
+// framework instantiation
 $app=require('lib/alit.php');
+// load config file
 $app->config('config.ini');
-
+// register a GET route to base url
 $app->route('GET /',function() use($app) {
+	// render the view
     Knife::instance()->render($app->get('myvar.view'),$app->get('myvar'));
 });
 
+// run the app
 $app->run();
